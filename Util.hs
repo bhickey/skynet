c7b6018fcbc6e736ddbe25683a05bebec71af206
Brendan Hickey <brendan@bhickey.net>
2011-10-31 20:32:34 -0700

    Added some unsafe printing functions. Don't side effect in 'em

--- Util.hs
+++ Util.hs
@@ -2,8 +2,13 @@ module Util
   ( fAnd
   , fOr
   , tuplify2
+  , dump
+  , output
   ) where
 
+import System.IO
+import System.IO.Unsafe
+ 
 fAnd :: a -> [a -> Bool] -> Bool
 fAnd x = all ($x)
 
@@ -16,3 +21,9 @@ tuplify2 [x,y] = (x,y)
 if' :: Bool -> a -> a -> a
 if' True x _ = x
 if' False _ y = y
+
+dump :: Show a => a -> a 
+dump a = output a a
+
+output :: Show a => a -> b -> b
+output a = seq (unsafePerformIO $ hPutStrLn stderr $ show a)
